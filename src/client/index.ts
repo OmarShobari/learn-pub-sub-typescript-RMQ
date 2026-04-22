f78ee9356d2ed121a582e59289aed1b998e753f1
OmarShobari <omar.shobari@gmail.com>
2026-04-22 10:49:06 +0200

    add handlerMove function and update index to integrate move handling

--- src/client/index.ts
+++ src/client/index.ts
@@ -11,14 +11,19 @@ import {
   SimpleQueueType,
   subscribeJSON,
 } from "../internal/pubsub/consume.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import {
+  ArmyMovesPrefix,
+  ExchangePerilDirect,
+  ExchangePerilTopic,
+  PauseKey,
+} from "../internal/routing/routing.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
-import { handlerPause } from "./handlers.js";
+import { handlerMove, handlerPause } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
 
 async function main() {
-  console.log("Starting Peril client...");
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   console.log("Peril game client connected to RabbitMQ!");
@@ -36,65 +41,64 @@ async function main() {
     }),
   );
 
-  const username = await clientWelcome().catch((err) => {
-    console.error("Error during client welcome:", err);
-    process.exit(1);
-  });
+  const username = await clientWelcome();
+  const gs = new GameState(username);
+  const publishCh = await conn.createConfirmChannel();
 
-  declareAndBind(
+  await subscribeJSON(
+    conn,
+    ExchangePerilTopic,
+    `${ArmyMovesPrefix}.${username}`,
+    `${ArmyMovesPrefix}.*`,
+    SimpleQueueType.Transient,
+    handlerMove(gs),
+  );
+
+  await subscribeJSON(
     conn,
     ExchangePerilDirect,
     `${PauseKey}.${username}`,
     PauseKey,
     SimpleQueueType.Transient,
+    handlerPause(gs),
   );
 
-  const gameState = new GameState(username);
-  subscribeJSON(
-    conn,
-    ExchangePerilDirect,
-    `${PauseKey}.${username}`,
-    PauseKey,
-    SimpleQueueType.Transient,
-    handlerPause(gameState),
-  );
   while (true) {
-    const words = await getInput("> ");
+    const words = await getInput();
     if (words.length === 0) {
       continue;
     }
-    const command = words[0]!.toLowerCase();
-
-    switch (command) {
-      case "spawn":
-        try {
-          commandSpawn(gameState, words);
-        } catch (err) {
-          console.error(err instanceof Error ? err.message : err);
-        }
-        break;
-      case "move":
-        try {
-          commandMove(gameState, words);
-        } catch (err) {
-          console.error(err instanceof Error ? err.message : err);
-        }
-        break;
-      case "status":
-        commandStatus(gameState);
-        break;
-      case "help":
-        printClientHelp();
-        break;
-      case "quit":
-        printQuit();
-        process.exit(0);
-      case "spam":
-        console.log("Spamming not allowed yet!");
-        break;
-      default:
-        console.error(`Unknown command: ${command}`);
-        break;
+    const command = words[0];
+    if (command === "move") {
+      try {
+        const move = commandMove(gs, words);
+        publishJSON(
+          publishCh,
+          ExchangePerilTopic,
+          `${ArmyMovesPrefix}.${username}`,
+          move,
+        );
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "status") {
+      commandStatus(gs);
+    } else if (command === "spawn") {
+      try {
+        commandSpawn(gs, words);
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "help") {
+      printClientHelp();
+    } else if (command === "quit") {
+      printQuit();
+      process.exit(0);
+    } else if (command === "spam") {
+      console.log("Spamming not allowed yet!");
+    } else {
+      console.log("Unknown command");
+      continue;
     }
   }
 }
