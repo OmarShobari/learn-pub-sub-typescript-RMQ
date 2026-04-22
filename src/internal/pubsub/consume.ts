311b2e2afe6153b151ddfdcc7d92531c74a7f4a0
OmarShobari <omar.shobari@gmail.com>
2026-04-22 21:53:31 +0200

    refactor import statements and add comments for clarity in consume module

--- src/internal/pubsub/consume.ts
+++ src/internal/pubsub/consume.ts
@@ -11,6 +11,7 @@ export enum SimpleQueueType {
   Transient,
 }
 
+// declareAndBind declares a queue and binds it to the specified exchange with the given routing key.
 export async function declareAndBind(
   conn: amqp.ChannelModel,
   exchange: string,
@@ -19,17 +20,20 @@ export async function declareAndBind(
   queueType: SimpleQueueType,
 ): Promise<[Channel, amqp.Replies.AssertQueue]> {
   const ch = await conn.createChannel();
-
   const queue = await ch.assertQueue(queueName, {
     durable: queueType === SimpleQueueType.Durable,
     exclusive: queueType !== SimpleQueueType.Durable,
     autoDelete: queueType !== SimpleQueueType.Durable,
+    arguments: {
+      "x-dead-letter-exchange": "peril_dlx",
+    },
   });
 
   await ch.bindQueue(queue.queue, exchange, key);
   return [ch, queue];
 }
 
+// subscribeJSON sets up a consumer that listens to messages on the specified exchange and queue.
 export async function subscribeJSON<T>(
   conn: amqp.ChannelModel,
   exchange: string,
